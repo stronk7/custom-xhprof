92e48777484f10745f0bff0039bbc5e263b2bdb3
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2013-02-22 01:02:45 +0100

    Customizations (always mimic from current moodle.git master)
    Basically:
    
    - add some basic styles.
    - include our own runner and replace the default one.
    - use own CFG setting to define the path to dot.

--- xhprof_html/callgraph.php
+++ xhprof_html/callgraph.php
@@ -29,6 +29,13 @@
  * @author Changhao Jiang (cjiang@facebook.com)
  */
 
+// Start moodle modification: moodleize this script.
+require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+require_once($CFG->libdir . '/xhprof/xhprof_moodle.php');
+require_login();
+require_capability('moodle/site:config', context_system::instance());
+// End moodle modification.
+
 // by default assume that xhprof_html & xhprof_lib directories
 // are at the same level.
 $GLOBALS['XHPROF_LIB_ROOT'] = dirname(__FILE__) . '/../xhprof_lib';
@@ -78,7 +85,10 @@ if (!array_key_exists($type, $xhprof_legal_image_types)) {
   $type = $params['type'][1]; // default image type.
 }
 
-$xhprof_runs_impl = new XHProfRuns_Default();
+// Start moodle modification: use own XHProfRuns implementation.
+// $xhprof_runs_impl = new XHProfRuns_Default();
+$xhprof_runs_impl = new moodle_xhprofrun();
+// End moodle modification.
 
 if (!empty($run)) {
   // single run call graph image generation
